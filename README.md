c7f3333c7ca0e348a836175c9ab2829626bfb730
xonecas <seancaetanomartin@gmail.com>
2026-01-13 20:57:54 +0000

    chore: cleanup and deps management

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Search Agent thingy (SAT)
 
 A high-performance local-first RAG (Retrieval-Augmented Generation) system built with Next.js, Ollama, and LangChain. The system uses PostgreSQL with pgvector for embeddings, Redis for caching, and SearXNG for meta-search retrieval. The frontend is built with Next.js App Router, shadcn/ui, and Tailwind CSS, while the backend uses Drizzle ORM for database interactions and Server Actions for data operations.
+
+## Docker setup
+
+See the [compose file](./docker-compose.yml)
+
+Start docker containers:
+```
+docker compose up -d
+```
+
+Stop docker containers:
+```
+docker compose down
+```
--- src/components/brand.tsx
+++ src/components/brand.tsx
@@ -4,7 +4,7 @@ import Logo from "@/app/favicon.svg";
 export function Brand() {
   return (
     <div className="p-8">
-      <div className="bg-foreground rounded-full p-1 drop-shadow-sm">
+      <div className="text-background bg-foreground rounded-full py-2 px-4 drop-shadow-sm flex flex-row items-center justify-center gap-2">
         <Image
           src={Logo}
           alt="Logo"
@@ -12,8 +12,8 @@ export function Brand() {
           height={32}
           className="invert dark:invert-0"
         />
+        <h1 className="text-2xl font-bold">SAT</h1>
       </div>
-      <h1 className="text-2xl font-bold pt-4">SAT</h1>
     </div>
   );
 }
